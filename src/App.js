05b55d09fd678f784996e841943c3dde8e29de30
Ϳαγ ᾥίῘῘίαm Sαρατ <jaywilliamsapar@gmail.com>
2023-05-14 08:52:55 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Logo from './Logo.png';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} className="App-logo" alt="logo" />
+        <p>
+          PHILIPPINES AIR FORCE 
+        </p>
+        <a
+          className="App-link"
+          href="https://www.paf.mil.ph/"
+          target="_blank"
+          rel="Philippines Air Force"
+        >
+          Learn more about PAF
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
